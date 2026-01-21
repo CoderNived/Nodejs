bf775fadd0ab96b3e304e736d190087f11637303
CoderNived <nivedshenoy0@gmail.com>
2026-01-21 21:37:05 +0530

    Tutorial 2

--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,5 @@
 console.log("Hello from nodeJS");
+const tutorial = require('./tutorial');
+console.log(tutorial)
+console.log(tutorial.sum(1,1))
+console.log(tutorial.pi)
---
+++ Basics/tutorial.js
@@ -0,0 +1,9 @@
+const sum = (num1,num2) => num1 + num2;
+const pi = 3.14;
+class SomeMathObject{
+    constructor(){
+        console.log("Object Created");
+    }
+}
+module.exports.sum=sum;
+module.exports.pi=pi;
